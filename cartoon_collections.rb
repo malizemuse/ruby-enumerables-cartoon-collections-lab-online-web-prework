1e1b379e320c301cfaf44c97c0fb48b85f108dc5
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-01 21:00:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,26 @@
 def roll_call_dwarves(array)
   
+<<<<<<< HEAD
   array.each_with_index { |dwarf, i| puts "#{i+1} #{dwarf}" }
+=======
+  array.each_with_index { |dwarf| puts dwarf}
+>>>>>>> 4cfdb17d36900f16729d00f113b5d3c0af1f2cb5
   
 end
 
 def summon_captain_planet(array)
   
+<<<<<<< HEAD
   array.collect { |call| call.capitalize + "!" }
+=======
+  new_array = []
+  
+  array.map { |call| new_array << "#{call.capitalize}!"}
+  
+  p new_array
+  
+  #new_array
+>>>>>>> 4cfdb17d36900f16729d00f113b5d3c0af1f2cb5
   
 end
 
@@ -22,5 +36,12 @@ def find_the_cheese(array)
 end
 
 
+<<<<<<< HEAD
 
 
+=======
+# Scrap Code
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+summon_captain_planet(planeteer_calls)
+>>>>>>> 4cfdb17d36900f16729d00f113b5d3c0af1f2cb5
